fc079a71fec461b6667618742dd89dc6818d5df7
Sacradd <Lackar@mail.ru>
2021-05-28 17:59:57 +0300

    added NavLink

--- components/Dialogs/Dialogs.jsx
+++ components/Dialogs/Dialogs.jsx
@@ -2,8 +2,18 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>Misha</div>
+                <div className={classes.dialog}>Jenek</div>
+                <div className={classes.dialog}>Serega</div>
+                <div className={classes.dialog}>Sanek</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How is your i-kamasutra</div>
+                <div className={classes.message}>Yo</div>
+            </div>
         </div>
     )
 }
